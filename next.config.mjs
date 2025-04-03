86c5f6e74ec5b90e4786d8bcc6cf141ac201fa64
Anthonycs96 <anthonycs.dev@gmail.com>
2025-04-03 14:45:18 -0500

    se alista archivos para despliegue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['scontent.flim23-1.fna.fbcdn.net'],
     },
+    output: 'export',
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 export default nextConfig;
