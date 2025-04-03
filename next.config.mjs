98db1495c17caf30032b477e03db673d5d5cc0a6
Anthonycs96 <anthonycs.dev@gmail.com>
2025-04-03 16:10:17 -0500

    preparando para despliegue

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,6 @@ const nextConfig = {
         domains: ['scontent.flim23-1.fna.fbcdn.net'],
         unoptimized: true,
     },
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/mi-portafolio' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/mi-portafolio/' : '',
 };
 
 export default nextConfig;
